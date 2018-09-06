208a2f592140fc8fb7830f20085f12712b36b6d6
yiling-hwong <yiling.hwong@gmail.com>
2018-09-06 16:10:40 +1000

    updated

---
+++ smep/static/maxchar.js
@@ -0,0 +1,11 @@
+$('textarea').keyup(function() {
+    
+  var characterCount = $(this).val().length,
+      current = $('#current'),
+      maximum = $('#maximum'),
+      theCount = $('#the-count');
+    
+  current.text(characterCount);
+ 
+    
+});
